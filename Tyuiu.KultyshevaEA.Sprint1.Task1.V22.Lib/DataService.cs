d4be6af39b39050ed811bc63fdc123cec5d16fe2
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-09-19 23:44:38 +0500

    Task1

---
+++ Tyuiu.KultyshevaEA.Sprint1.Task1.V22.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KultyshevaEA.Sprint1.Task1.V22.Lib
+{
+    public class DataService : ISprint1Task1V22
+    {
+        public double Calculate(double x, double y)
+        {
+            return (5 + x) / (y * x);
+        }
+    }
+}
---
+++ Tyuiu.KultyshevaEA.Sprint1.Task1.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KultyshevaEA.Sprint1.Task1.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KultyshevaEA.Sprint1.Task1.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0fdce20-6e9b-47f8-9464-6bd5193f47ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
