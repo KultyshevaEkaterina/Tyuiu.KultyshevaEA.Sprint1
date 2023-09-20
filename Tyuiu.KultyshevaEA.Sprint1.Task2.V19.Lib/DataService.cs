3c35bf2332166c4fe14b2b628cb3fcaff89e772b
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-09-20 21:16:13 +0500

    Task2

---
+++ Tyuiu.KultyshevaEA.Sprint1.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KultyshevaEA.Sprint1.Task2.V19.Lib
+{
+    public class DataService : ISprint1Task2V19
+    {
+        public double ConvertInchToKm(int value)
+        {
+            return value * 0.0254;
+        }
+    }
+}
---
+++ Tyuiu.KultyshevaEA.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KultyshevaEA.Sprint1.Task2.V19.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 100;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(2.54, res);
+        }
+    }
+}
