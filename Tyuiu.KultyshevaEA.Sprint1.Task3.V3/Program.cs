b03cc04038c3c3a67801379359f527cd99bec6fe
Kultysheva Ekaterina <kultysheva.ekaterina@gmail.com>
2023-09-22 01:22:51 +0500

    Task3

---
+++ Tyuiu.KultyshevaEA.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Tyuiu.KultyshevaEA.Sprint1.Task3.V3.Lib;
+
+namespace Tyuiu.KultyshevaEA.Sprint1.Task3.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Култышева Е. А. | ИИПБ-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Култышева Екатерина Александровна | ИИПБ-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int lenght;
+            int widht;
+            int height;
+
+            Console.WriteLine("Введите длину:");
+            lenght = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите ширину:");
+            widht = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите высоту:");
+            height = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Объем в см.куб. = " + ds.ParallelepipedVolume(lenght,widht,height));
+
+            Console.ReadLine();
+        }
+    }
+}
